40e07a688e1e4483bad74727437ead565edd4a31
NeroSH <35072252+NeroSH@users.noreply.github.com>
2026-07-26 11:26:12 +0500

    test: CI gate to incoming PRs (#1)
    Added Github CI Actions to check PRs quality by at least unit testing before merging
    
    ---------
    
    Signed-off-by: NeroSH <mustafo.xon@gmail.com>

---
+++ core/policy/src/test/kotlin/com/shdev/guardian/policy/CiGateProbeTest.kt
@@ -0,0 +1,9 @@
+package com.shdev.guardian.policy
+
+import kotlin.test.Test
+import kotlin.test.assertEquals
+
+class CiGateProbeTest {
+    @Test
+    fun probe() = assertEquals(expected = 2, actual = 2, message = "CI gate probe")
+}
